db7b88b47feaa985f37c3bc3e39511aa9e6b971a
gwonyejun <kyjuni1059@gmail.com>
2023-09-17 17:07:36 +0900

    player 움직이게 설정 해보기

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-  
+    [SerializeField]
+    private float moveToSpeed;
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("horizontal");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveTo = new Vector3(horizontalInput, verticalInput, 0f);
+        transform.position += moveTo * moveToSpeed * Time.deltaTime;
     }
 }

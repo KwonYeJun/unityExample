d8a38badd821755a9d080f2abafc62ef0e99ea82
gwonyejun <kyjuni1059@gmail.com>
2023-09-17 18:48:34 +0900

    정상적으로 작동 되는 것을 확인 하였다.

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -7,13 +7,13 @@ public class Background : MonoBehaviour
     // 이곳에 Background 컴포넌트의 동작과 속성을 정의합니다.
     private float moveSpeed = 5.05f;
      // 'f' 접미사를 사용하여 float 형식임을 나타내주어야 실수로 사용이 가능하다.
-    private int check = 0;
+    // private int check = 0;
     // Update is called once per frame
     void Update()
     {
         // 게임이 실행 되는 동안 자동으로 호출이 되는 메서드 이다.
         // 이곳에 매 프레임마다 실행되는 코드를 작성합니다.
-            Debug.Log(moveSpeed);
+            // Debug.Log(moveSpeed);
         transform.position += Vector3.down * moveSpeed * Time.deltaTime;
         if (transform.position.y < -10)
         {

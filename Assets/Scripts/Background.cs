73e7ac1e6ce708fa6417beb61711c1977cc39563
gwonyejun <kyjuni1059@gmail.com>
2023-09-17 16:57:00 +0900

    배경 위치 변화시에 다시 원래 위치로 이동 할 수 있게 만들어 주기기

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -9,7 +9,12 @@ public class Background : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 게임이 실행 되는 동안 자동으로 호출이 되는 메서드 이다.
         // 이곳에 매 프레임마다 실행되는 코드를 작성합니다.
-        transform.position += new Vector3.down * moveSpeed;
+        transform.position += Vector3.down * moveSpeed;
+        if(transform.position.y <  -17){
+        transform.position += new Vector3(0, 34f);
+        }
+
     }
 }

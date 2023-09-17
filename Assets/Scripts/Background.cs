565c2ff03856e7ca69774cd8781ed2379c010c78
gwonyejun <kyjuni1059@gmail.com>
2023-09-17 17:50:49 +0900

    일정 이상이 지나면 지나가는 속도를 증가 시켜서 빠르기 효과를 줘 보았다.

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class Background : MonoBehaviour
 {
     // 이곳에 Background 컴포넌트의 동작과 속성을 정의합니다.
-    private float moveSpeed = 3;
+    private float moveSpeed = 0.5f;
+     // 'f' 접미사를 사용하여 float 형식임을 나타내주어야 실수로 사용이 가능하다.
+    private int check = 0;
     // Update is called once per frame
     void Update()
     {
         // 게임이 실행 되는 동안 자동으로 호출이 되는 메서드 이다.
         // 이곳에 매 프레임마다 실행되는 코드를 작성합니다.
         transform.position += Vector3.down * moveSpeed;
-        if(transform.position.y <  -17){
-        transform.position += new Vector3(0, 34f);
+        if (transform.position.y < -17)
+        {
+            transform.position += new Vector3(0, 34f);
+            check++;
+        }
+
+        if (check >= 20)
+        {
+            moveSpeed += 0.1f;
+             check = 0; 
         }
 
     }

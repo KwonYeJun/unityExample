e5dc9442e9e71d8ea0b71103540b9d5bec1883d6
gwonyejun <kyjuni1059@gmail.com>
2023-09-17 18:27:48 +0900

    moveSpeed의 속도를 증가 시키면 맵이 밀리는걸 확인 하였다.

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Background : MonoBehaviour
 {
     // 이곳에 Background 컴포넌트의 동작과 속성을 정의합니다.
-    private float moveSpeed = 0.5f;
+    private float moveSpeed = 5.05f;
      // 'f' 접미사를 사용하여 float 형식임을 나타내주어야 실수로 사용이 가능하다.
     private int check = 0;
     // Update is called once per frame
@@ -13,18 +13,19 @@ public class Background : MonoBehaviour
     {
         // 게임이 실행 되는 동안 자동으로 호출이 되는 메서드 이다.
         // 이곳에 매 프레임마다 실행되는 코드를 작성합니다.
-        transform.position += Vector3.down * moveSpeed;
-        if (transform.position.y < -17)
+            Debug.Log(moveSpeed);
+        transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        if (transform.position.y < -10)
         {
-            transform.position += new Vector3(0, 34f);
-            check++;
+            transform.position += new Vector3(0, 20f, 0);
+            // check++;
         }
 
-        if (check >= 20)
-        {
-            moveSpeed += 0.1f;
-             check = 0; 
-        }
+        // if (check >= 5)
+        // {
+        //     moveSpeed += 1.18f;
+        //      check = 0; 
+        // }
 
     }
 }

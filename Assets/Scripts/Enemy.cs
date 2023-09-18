f8ce4d8cea75238626e1135e6752123dda432319
gwonyejun <kyjuni1059@gmail.com>
2023-09-18 21:35:57 +0900

    장애물 떨어지게 작성

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
 
+ //public를 하게 되면 그냥 사용이 가능하다.
+    [SerializeField]
+    private float moveSpeed = 5f;
     // Update is called once per frame
+    private float minY = -7f;
     void Update()
     {
+     
+        transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        if(transform.position.y < minY){
+            Destroy(gameObject);
+        }
+    }
+ // Start is called before the first frame update
+    void Start()
+    {
+      
         
     }
 }

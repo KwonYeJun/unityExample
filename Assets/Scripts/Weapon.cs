8f12c57654595607947df6a42c9c59d96fbeb8f8
gwonyejun <kyjuni1059@gmail.com>
2023-09-18 21:43:54 +0900

    실수 f 처리

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,7 +6,7 @@ public class Weapon : MonoBehaviour
 {
     //public를 하게 되면 그냥 사용이 가능하다.
     [SerializeField]
-    private float moveSpeed = 20;
+    private float moveSpeed = 20f;
     // Update is called once per frame
     void Update()
     {
